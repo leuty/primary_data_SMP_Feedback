3c5b140a9c7e6deb759a2877b4053cdd049a0cc6
davidle <david.leutwyler@env.ethz.ch>
2020-06-02 16:47:22 +0200

    Plots Fig8

---
+++ plots/sh/plot_pertsize_wind.sh
@@ -0,0 +1,79 @@
+#!/bin/bash -l
+
+. /etc/bash.bashrc
+module load daint-gpu
+module load NCL 
+ 
+
+PROJECT=/project/pr04/davidle
+shdir=/users/davidle/analysis_soilmoistre_pertubation_10a/ncl
+if [ ! -d $plotdir ]; then mkdir -p $plotdir; fi
+
+datadir=/project/pr04/davidle/results_soil-moisture_pertubation/analysis
+
+#Precip
+#--------------
+for LM_MODEL in lm_c lm_f; do
+
+
+  #lm_f_domain
+  #---------------
+  if [ $LM_MODEL == lm_c ]; then
+    cdata_model=$PROJECT/results_clim/lm_c/lffd1993110100c.nc
+  else
+    cdata_model=$PROJECT/results_clim/lm_f/lffd1998110100c.nc
+  fi
+
+  #Windspeed
+  data=$datadir/wet25-dry25_convdays_200607_lm_f_domain/${LM_MODEL}_wet25_convdays_200607p_timmean.nc
+  plotdir=/project/pr04/davidle/results_soil-moisture_pertubation/analysis/plots_lm_f_domain
+  plotname=$plotdir/$LM_MODEL/windspeed
+  $shdir/sh/pass_args.sh $shdir/U_mag.ncl $data $cdata_model $plotname
+
+  #Difference in windspeed
+  data=$datadir/wet25-dry25_convdays_200607_lm_f_domain/${LM_MODEL}_diff_convdays_200607p_timmean.nc
+  plotdir=/project/pr04/davidle/results_soil-moisture_pertubation/analysis/plots_lm_f_domain
+  plotname=$plotdir/$LM_MODEL/diff_windspeed
+  $shdir/sh/pass_args.sh $shdir/U_mag.ncl $data $cdata_model $plotname
+
+
+  #Alps
+  #---------------
+  if [ $LM_MODEL == lm_c ]; then
+    cdata_model=$PROJECT/results_clim/lm_c/lffd1993110100c.nc
+  else
+    cdata_model=$PROJECT/results_clim/lm_f/lffd1998110100c.nc
+  fi
+
+  #Windspeed
+  data=$datadir/wet25-dry25_convdays_200607_Alps/${LM_MODEL}_wet25_convdays_200607p_timmean.nc
+  plotdir=/project/pr04/davidle/results_soil-moisture_pertubation/analysis/plots_Alps
+  plotname=$plotdir/$LM_MODEL/windspeed_zoom
+  $shdir/sh/pass_args.sh $shdir/U_mag_zoom.ncl $data $cdata_model $plotname
+
+  #Difference in windspeed
+  data=$datadir/wet25-dry25_convdays_200607_Alps/${LM_MODEL}_diff_convdays_200607p_timmean.nc
+  plotdir=/project/pr04/davidle/results_soil-moisture_pertubation/analysis/plots_Alps
+  plotname=$plotdir/$LM_MODEL/diff_windspeed_zoom
+  $shdir/sh/pass_args.sh $shdir/U_mag_zoom.ncl $data $cdata_model $plotname
+
+
+#  #Cathy lm_f
+#  #----------
+#
+#  #Windspeed
+#  cdata_model=$PROJECT/results_soil-moisture_pertubation/wet25_cathy_lm_f/2006/${LM_MODEL}/1h/lffd2006070100c.nc
+#  data=$datadir/wet25-dry25_convdays_200607_cathy_lm_f/${LM_MODEL}_dry25_convdays_200607p_timmean.nc
+#  plotdir=/project/pr04/davidle/results_soil-moisture_pertubation/analysis/plots_cathy_lm_f
+#  plotname=$plotdir/$LM_MODEL/windspeed_zoom
+#  $shdir/sh/pass_args.sh $shdir/U_mag_zoom.ncl $data $cdata_model $plotname
+#
+#  #Difference in Windspeed
+#  cdata_model=$PROJECT/results_soil-moisture_pertubation/wet25_cathy_lm_f/2006/${LM_MODEL}/1h/lffd2006070100c.nc
+#  data=$datadir/wet25-dry25_convdays_200607_cathy_lm_f/${LM_MODEL}_diff_convdays_200607p_timmean.nc
+#  plotdir=/project/pr04/davidle/results_soil-moisture_pertubation/analysis/plots_cathy_lm_f
+#  plotname=$plotdir/$LM_MODEL/diff_windspeed_zoom
+#  $shdir/sh/pass_args.sh $shdir/U_mag_zoom.ncl $data $cdata_model $plotname
+
+done
+
